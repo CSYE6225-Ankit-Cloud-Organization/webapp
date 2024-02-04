4baef33ecb2275a32acba1aa59b2fed26a0550c8
Ankit Goyal <114370700+goyalankit28@users.noreply.github.com>
2024-02-04 16:33:24 -0500

    trying the workflow test

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -5,11 +5,18 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize({
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOSTNAME,
-    "dialect": process.env.DB_DIALECT
+    "username": postgres,
+    "password": postgres,
+    "database": postgres,
+    "host": localhost,
+    "dialect": postgres
 });
+// const sequelize = new Sequelize({
+//     "username": process.env.DB_USER,
+//     "password": process.env.DB_PASSWORD,
+//     "database": process.env.DB_NAME,
+//     "host": process.env.DB_HOSTNAME,
+//     "dialect": process.env.DB_DIALECT
+// });
 // const User = require('../models/User'); // Import the User model
 module.exports = sequelize;
