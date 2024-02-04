3880a11110a8daecaeb4bae6909f23a2d8b26769
Ankit Goyal <114370700+goyalankit28@users.noreply.github.com>
2024-02-04 16:26:09 -0500

    trying the workflow test

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //Includes all server side code for the web application...
 const app = require('./src/app');
-const PORT = process.env.APP_PORT || 3000; // picking the port details from env file or default to 3000
+// const PORT = process.env.APP_PORT || 3000; // picking the port details from env file or default to 3000
 // const sequelize = require('./src/config/dbConnection');
 // const User = require('./src/models/User'); // Import the User model
 
