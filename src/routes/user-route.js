94f4a0d3540c953c2a0702f28004e2c1c67dc250
Ankit Goyal <114370700+goyalankit28@users.noreply.github.com>
2024-02-07 14:03:35 -0500

    corrections in controller and route file

--- src/routes/user-route.js
+++ src/routes/user-route.js
@@ -8,7 +8,7 @@ userRouter.head('/self', (req, res) => {
 });
 userRouter.get('/self', validations.checkEmptyPayload, validations.checkContentType, validations.checkAuthorizationFields, validations.checkDbhealth, userController.getUser);
 userRouter.put('/self', validations.checkContentType, validations.checkAuthorizationFields, validations.checkDbhealth, userController.updateUser);
-userRouter.post('', validations.checkContentType, validations.checkDbhealth, userController.createUser);
+userRouter.post('', validations.checkContentType, userController.createUser);
 
 
 //explicitly handle all other api call options
