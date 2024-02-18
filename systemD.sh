7f7e683716286fd9466579f8481122567c07e9f2
Ankit Goyal <114370700+goyalankit28@users.noreply.github.com>
2024-02-18 01:39:24 -0500

    All errors fixed

--- systemD.sh
+++ systemD.sh
@@ -5,10 +5,3 @@ sudo systemctl daemon-reload
 sudo systemctl enable startnode
 sudo systemctl start startnode
 sudo systemctl status startnode
-APISRVC=$?
-
-if [ $APISRVC -eq 0 ]; then
-  echo "success"
-else
-  echo "fail"
-fi
