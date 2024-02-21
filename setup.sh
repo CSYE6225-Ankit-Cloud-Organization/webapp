0a30c111e705a5a7201e1dff385dc6106d264910
Ankit Goyal <114370700+goyalankit28@users.noreply.github.com>
2024-02-21 12:36:15 -0500

    fixing bugs in script

--- setup.sh
+++ setup.sh
@@ -31,10 +31,10 @@ echo "postgres service is setup succesfully"
 #Setup the Postgres db
 username=$DB_USERNAME
 password=$DB_PASSWORD
-dbname=$DB_NAME
+databasename=$DATABASE_NAME
 
 echo "postgres db commands starting execuingnjgn successfully"
 sudo -i -u postgres psql -c "ALTER USER $username WITH PASSWORD '$password';"
-sudo -i -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $dbname TO $username;"
+sudo -i -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $databasename TO $username;"
 sudo -i -u postgres psql -c "GRANT ALL ON SCHEMA public TO $username;"
 echo "postgres db commands executed successfully"
