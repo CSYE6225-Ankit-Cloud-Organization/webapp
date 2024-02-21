13a4e7e208f5cec508e16e17dfea6d72ad094186
Ankit Goyal <114370700+goyalankit28@users.noreply.github.com>
2024-02-21 14:48:22 -0500

    testing image build

--- setupphase2.sh
+++ setupphase2.sh
@@ -5,11 +5,11 @@ sudo useradd -s /usr/sbin/nologin -g csye6225 -d /opt/csye6225 -m csye6225
 
 cd /opt || exit
 sudo unzip webapp.zip -d csye6225
-sudo -u csye6225 bash
+sudo -u csye6225 bash -c "cd /opt/csye6225 && npm install && rm -rf .git && exit"
 # sudo chmod o+rx /opt/csye6225
 pwd
-ls -al /opt/csye6225
-cd /opt/csye6225 || exit
-sudo npm install
-sudo rm -rf .git
+# ls -al /opt/csye6225
+# cd /opt/csye6225 || exit
+# sudo npm install
+# sudo rm -rf .git
 
