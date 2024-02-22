d893c74c46916ef57806ed55cc402efd35024b7b
Ankit Goyal <114370700+goyalankit28@users.noreply.github.com>
2024-02-21 20:06:31 -0500

    error fix

--- setupphase2.sh
+++ setupphase2.sh
@@ -5,8 +5,9 @@ sudo useradd -s /usr/sbin/nologin -g csye6225 -d /opt/csye6225 -m csye6225
 
 cd /opt || exit
 sudo unzip webapp.zip -d csye6225
-sudo -u csye6225 bash -c "cd /opt/csye6225 && npm install && rm -rf .git && exit"
+sudo chmod o+rx /opt/csye6225
 pwd
-
-
+ls -al /opt/csye6225
+cd /opt/csye6225 || exit
+sudo npm install
 
