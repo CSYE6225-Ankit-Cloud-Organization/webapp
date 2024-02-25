3fca3e29983d9bbda7c1af9ba0a465783276098e
Ankit Goyal <114370700+goyalankit28@users.noreply.github.com>
2024-02-24 22:18:13 -0500

    fixing image errors

--- systemD.sh
+++ systemD.sh
@@ -3,5 +3,5 @@
 echo "Enabling npm start service"
 sudo systemctl daemon-reload
 sudo systemctl enable startnode
-sudo systemctl start startnode
+# sudo systemctl start startnode
 # sudo systemctl status startnode
