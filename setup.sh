3514209c4671a4e9ae30d4b08d238e8697b662f4
Ankit Goyal <114370700+goyalankit28@users.noreply.github.com>
2024-02-24 20:19:16 -0500

    removing postgres

--- setup.sh
+++ setup.sh
@@ -14,22 +14,22 @@ sudo dnf module install nodejs:20 -y
 sudo dnf install npm -y
 sudo dnf install unzip -y
 sudo dnf remove git -y
-sudo dnf install postgresql-server -y
-sudo dnf install postgresql-contrib -y
+# sudo dnf install postgresql-server -y
+# sudo dnf install postgresql-contrib -y
 echo "packages are updated"
 
 # Setup PostgreSQL
-sudo postgresql-setup initdb
-sudo systemctl start postgresql
-sudo systemctl stop postgresql
-sudo sed -i -E '/^host\s+all\s+all\s+(127\.0\.0\.1\/32|::1\/128)\s+ident$/ s/ident/md5/' /var/lib/pgsql/data/pg_hba.conf
-sudo systemctl enable postgresql
-sudo systemctl start postgresql
-sudo systemctl status postgresql
-echo "postgres service is setup succesfully"
+# sudo postgresql-setup initdb
+# sudo systemctl start postgresql
+# sudo systemctl stop postgresql
+# sudo sed -i -E '/^host\s+all\s+all\s+(127\.0\.0\.1\/32|::1\/128)\s+ident$/ s/ident/md5/' /var/lib/pgsql/data/pg_hba.conf
+# sudo systemctl enable postgresql
+# sudo systemctl start postgresql
+# sudo systemctl status postgresql
+# echo "postgres service is setup succesfully"
 
 #Setup the Postgres db
-username=$DB_USERNAME
-password=$DB_PASSWORD
-sudo -i -u postgres psql -c "ALTER USER $username WITH PASSWORD '$password';"
-echo "postgres db commands executed successfully"
+# username=$DB_USERNAME
+# password=$DB_PASSWORD
+# sudo -i -u postgres psql -c "ALTER USER $username WITH PASSWORD '$password';"
+# echo "postgres db commands executed successfully"
